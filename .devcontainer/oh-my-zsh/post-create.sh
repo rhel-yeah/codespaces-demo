01e3294b1056685158bf101b1c05418409395fb4
Josh Garverick <josh.garverick@gmail.com>
2024-03-18 02:56:39 +0000

    Updating Dockerfile and removing local features

--- .devcontainer/oh-my-zsh/post-create.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo '[INFO] Copying p10k.zsh...'
-cp ./oh-my-zsh/p10k.zsh ~/.p10k.zsh
-
-echo '[INFO] Setting up oh-my-zsh...'
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
-echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc
